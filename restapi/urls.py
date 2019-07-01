53f251cd614620ab033d3956c0f892680548d0f9
abhisek11 <abhisekrock94@gamil.com>
2019-07-02 00:57:37 +0530

    serialize

--- restapi/urls.py
+++ restapi/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('json/cbv/',JsonCBV.as_view()),
     path('json/cbv2/',JsonCBV2.as_view()),
-
+    path('json/serialized/list/',SerilizedListView.as_view()),
     path('update_view/',update_model_detail_view),
 ]
--- updates/views.py
+++ updates/views.py
@@ -1,4 +1,6 @@
 import json
+
+from django.core.serializers import serialize
 from django.http import JsonResponse,HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -35,3 +37,28 @@ class JsonCBV2(JsonResponseMixin,View):
             "content":"some new content"
         }
         return self.render_to_json_response(data)
+
+class SerilizedDetailView(View):
+    def get(self,request,*args,**kwargs):
+
+        obj=Update.object.get(id=1)
+        data ={
+            "user":obj.user.username,
+            "content": obj.content
+        }
+        json_data=json.dumps(data)
+        return HttpResponse(json_data,content_type='application/json') 
+
+
+class SerilizedListView(View):
+    def get(self,request,*args,**kwargs):
+
+        qs=Update.objects.all()
+        data = serialize("json",qs,fields=('user','content'))
+        print(data)
+        # data ={
+        #     "user":obj.user.username,
+        #     "content": obj.content
+        # }
+        # json_data=json.dumps(data)
+        return HttpResponse(data,content_type='application/json') 
